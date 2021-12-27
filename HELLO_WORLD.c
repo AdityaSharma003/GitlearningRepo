a837f91732ab4e3395a2e14644c0b363d6d514b4
AdityaSharma003 <sharma8090100@gmail.com>
2021-12-27 13:38:11 +0530

    third commit

--- HELLO_WORLD.c
+++ HELLO_WORLD.c
@@ -3,5 +3,10 @@
 int main(){
 
     printf("Hello World!");
+    
+    printf("Hello World   1   !");
+    printf("Hello World   2       !");
+    printf("Hello World   3           !");
+
     return 0;
 }
