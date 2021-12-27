67a7643f342a9cbed65791e5a6446b23be809202
AdityaSharma003 <sharma8090100@gmail.com>
2021-12-27 13:22:21 +0530

    Initial commit

---
+++ if_loops.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    int n;
+    printf("Enter the strength of class= ");
+    scanf("%d",&n);
+    if (n>20)
+    {
+    printf("Hurray Virus is gone!");
+    }
+    else{
+         printf("Virus is not gone!");
+    }
+     
+    return 0;
+}
